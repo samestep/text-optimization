d895e3c8b2db537cc393949af267c9467b7edc96
Sam Estep <sam@samestep.com>
2023-12-14 13:46:12 -0500

    Omit glyphs with multiple outer boundary components

--- common.ts
+++ common.ts
@@ -3,3 +3,9 @@ export type Segment = [string, ...number[]];
 export const strings = new Map<string, string>(
   Object.entries(JSON.parse(await Bun.file("letters.json").text())),
 );
+
+// our implementation assumes that the outer boundary of the glyph is connected,
+// so here we delete all the letters which violate that assumption
+strings.delete("lower_i");
+strings.delete("lower_j");
+strings.delete("upper_Xi");
