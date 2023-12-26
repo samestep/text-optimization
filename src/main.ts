7496dc124d293b7c2318976aff0a0ce28e13e7a8
Sam Estep <sam@samestep.com>
2023-12-26 14:08:33 -0500

    Make a canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log(document.getElementById("hello")?.innerText);
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+const setSize = () => {
+  const html = document.documentElement;
+  canvas.width = html.clientWidth;
+  canvas.height = html.clientHeight;
+};
+setSize();
+window.onresize = setSize;
+
+const ctx = canvas.getContext("2d")!;
+const draw = () => {
+  const { width, height } = canvas;
+
+  ctx.strokeStyle = "red";
+  ctx.strokeRect(5, 5, width - 10, height - 10);
+
+  window.requestAnimationFrame(draw);
+};
+window.requestAnimationFrame(draw);
